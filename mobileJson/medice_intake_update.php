4ebf8683330c924d8dcbb86312ba2577990fe1b7
Jiten Sabharwal <jiten@chalkstreet>
2016-01-12 14:54:04 +0530

    webservice updated

--- mobileJson/medice_intake_update.php
+++ mobileJson/medice_intake_update.php
@@ -2,11 +2,11 @@
 	include("../classes/database.php");
 ?>
 <?php
-	if(isset($_REQUEST['intakemedicine']))
+	if(isset($_REQUEST['intake']))
 	{
 		# write code to decode json
 		//$intake=json_decode($_REQUEST['intakemedicine'],true);
-		$nikshay_no=$_REQUEST['nikshay_no'];
+		$nikshay_no=$_REQUEST['Nikshay_no'];
 		#$query="UPDATE medicine_intake set medicine='1' where nikshay_no='$nikshay_no'";
 		$query="INSERT INTO  medicine_intake (Nikshay_no,intake) VALUES('$nikshay_no','True')";
 
